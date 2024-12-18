0c169bf1cb07b12d49128918b6918d476a2356e8
devi9113624552 <dpraidola@gmail.com>
2024-12-18 21:40:28 +0530

    added codes in ref project

--- refs/src/App.jsx
+++ refs/src/App.jsx
@@ -1,10 +1,16 @@
-import Player from './components/Player.jsx';
+import Player from "./components/Player.jsx";
+import TimerChallenge from "./components/TimerChallenge.jsx";
 
 function App() {
   return (
     <>
       <Player />
-      <div id="challenges"></div>
+      <div id="challenges">
+        <TimerChallenge title={"easy"} targetTime={1} />
+        <TimerChallenge title={"not easy"} targetTime={2} />
+        <TimerChallenge title={"tough"} targetTime={5} />
+        <TimerChallenge title={"pro's only"} targetTime={10} />
+      </div>
     </>
   );
 }
--- refs/src/components/Player.jsx
+++ refs/src/components/Player.jsx
@@ -1,23 +1,31 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 export default function Player() {
+  const playerName = useRef();
   const [enteredPlayerName, setEnteredPlayerName] = useState("");
-  const [submitted, setSubmitted] = useState(false);
+  //const [submitted, setSubmitted] = useState(false);
 
-  const handleChange = (event) => {
-    setSubmitted(false);
-    setEnteredPlayerName(event.targer.value);
-  };
+  // const handleChange = (event) => {
+  //   setSubmitted(false);
+  //   setEnteredPlayerName(event.targer.value);
+  // };
 
   const handleClick = () => {
-    setSubmitted(true);
+    setEnteredPlayerName(playerName.current.value);
+    playerName.current.value = "";
+    //setSubmitted(true);
   };
 
   return (
     <section id="player">
-      <h2>Welcome {submitted ? enteredPlayerName : "unknown entity"}</h2>
+      <h2>Welcome {enteredPlayerName ?? "unknown entity"}</h2>
       <p>
-        <input type="text" onChange={handleChange} value={enteredPlayerName} />
+        <input
+          ref={playerName}
+          type="text"
+          // onChange={handleChange}
+          //value={enteredPlayerName}
+        />
         <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
